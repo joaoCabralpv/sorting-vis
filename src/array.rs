75fce4a774d040d0c6601f2256dda438f0a8f516
asus <joaocabralpv@gmail.com>
2024-08-10 14:55:26 +0000

    Array rendering and improvement on test sorting algotithm

--- src/array.rs
+++ src/array.rs
@@ -1,24 +1,44 @@
+use core::f32;
+use std::thread::sleep;
+use std::time::Duration;
 use std::vec::Vec;
+
+use macroquad::color;
+use macroquad::prelude::*;
+
+enum AccessType {
+    Reading,
+    Writing
+
+}
 pub struct Array {
     array: Vec<i64>,
+    accessing:usize,
+    access_type: AccessType,
     n_reads: u128,
     n_writes: u128,
 }
 
 
 impl Array{
-    pub fn get_element(&mut self,index:usize) -> i64 {
+    pub async fn get_element(&mut self,index:usize) -> i64 {
         self.n_reads+=1;
+        self.accessing = index;
+        self.access_type = AccessType::Reading;
+        self.render().await;
         self.array[index].clone()
     }
-    pub fn set_element(&mut self,index:usize,element:i64) {
+    pub async fn set_element(&mut self,index:usize,element:i64) {
         self.n_writes+=1;
+        self.accessing = index;
+        self.access_type = AccessType::Writing;
         self.array[index] = element;
+        self.render().await;
     }
     pub fn new(array:Vec<i64>) -> Array {
-        Array { array: array, n_reads: 0, n_writes: 0 }
+        Array { array: array, accessing: 0, access_type: AccessType::Reading, n_reads: 0, n_writes: 0 }
     }
-    pub fn get_array(&self) -> &Vec<i64>{
+    pub async fn get_array(&self) -> &Vec<i64>{
         &self.array
     }
     pub fn len(&self) -> usize{
@@ -29,5 +49,22 @@ impl Array{
     }
     pub fn get_writes(&self) -> u128 {
         self.n_writes
+    }
+    pub async fn render(&mut self) {
+        let rect_width = screen_width() / self.array.len() as f32;
+        let rect_scale = screen_height()  / self.array.iter().max().expect("Array is empty").clone() as f32;
+        for i in 0..self.array.len() {
+            let color = match i == self.accessing {
+                true => match self.access_type {
+                    AccessType::Reading => GREEN,
+                    AccessType::Writing => RED,
+                },
+                false => color::WHITE,
+            };
+
+            draw_rectangle((i as f32*rect_width) as f32, screen_height()-(rect_scale*self.array[i]as f32), rect_width as f32 , f32::MAX, color); //Why calculate the height when you can just use f32::MAX lol
+        }
+        next_frame().await;
+        //println!("{:?}",self.array);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 mod array;
 use macroquad::prelude::*;
 use array::Array;
-use std::{time, vec::Vec};
+use std::vec::Vec;
 use rand::RandomRange;
 
 
-fn sort(mut array:Array)-> Array{
+async fn sort(mut array:Array)-> Array{
     let mut sorted = false;
+    let mut n_rounds:usize = 0;
     while !sorted {
         sorted = true;
-        for i in 0..array.len()-1{
-            if array.get_element(i) > array.get_element(i+1){
-                let e0 = array.get_element(i);
-                let e1 = array.get_element(i+1);
-                array.set_element(i, e1);
-                array.set_element(i+1,e0);
+        n_rounds+=1;
+        for i in 0..(array.len()-n_rounds){
+            if array.get_element(i).await > array.get_element(i+1).await{
+                let e0 = array.get_element(i).await;
+                let e1 = array.get_element(i+1).await;
+                array.set_element(i, e1).await;
+                array.set_element(i+1,e0).await;
                 sorted  = false;
             }
         }
@@ -26,18 +28,18 @@ fn sort(mut array:Array)-> Array{
 #[macroquad::main("sorting vis")]
 
 async fn main() {
-    //request_new_screen_size(1280.0, 720.0);
+    request_new_screen_size(1280.0, 720.0);
     
     rand::srand(std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).expect("Please use this program after 00:00:00 UTC 0 January 1st 1970").as_secs());
 
     let mut vec = Vec::new();
-    for _ in 0..100000 {
-        vec.push(RandomRange::gen_range(0, 100000))
+    for _ in 0..1000 {
+        vec.push(RandomRange::gen_range(0, 3000))
     }
     println!("1");
     let mut arr = Array::new(vec);
     let t = std::time::SystemTime::now();
-    arr = sort(arr);
+    arr = sort(arr).await;
     println!("Time: {}s",std::time::SystemTime::now().duration_since(t).expect("Time went backwards").as_secs_f64());
     println!("Reads: {}",arr.get_reads());
     println!("Writes: {}", arr.get_writes());
